d8478cee455cac3eec5cacf1513be194571c202b
YesMan20 <119973212+YesMan20@users.noreply.github.com>
2023-07-31 22:48:39 -0400

    Slight reorganization

--- src/main/java/net/yesman/scpff/level/item/ModItems.java
+++ src/main/java/net/yesman/scpff/level/item/ModItems.java
@@ -21,6 +21,14 @@ public class ModItems {
     public static final RegistryObject<Item> SCP500 = ITEMS.register("scp500", () -> new SCP500Item(new Item.Properties()
             .food(ModFoods.SCP500).stacksTo(7)));
 
+    public static final RegistryObject<Item> SCP1025 = ITEMS.register("scp1025", () -> new SCP1025Item(new Item.Properties()
+            .stacksTo(1)));
+
+    public static final RegistryObject<Item> SCP109 = ITEMS.register("scp109", () -> new SCP109Item(new Item.Properties()
+            .stacksTo(1).food(ModFoods.SCP109)));
+
+    public static final RegistryObject<Item> SCP268 = ITEMS.register("scp268", () -> new SCP268ArmorItem(ModArmorMaterials.SCP_268, ArmorItem.Type.HELMET, new Item.Properties()));
+
     /** Icons for tabs **/
 
     public static final RegistryObject<IgnoreOnLoad> ITEMICON = ITEMS.register("itemicon", () -> new IgnoreOnLoad(new Item.Properties()));
@@ -47,6 +55,20 @@ public class ModItems {
 
     public static final RegistryObject<Item> KEYCARD5 = ITEMS.register("keycard5", () -> new KeycardItem(new Item.Properties()
             .stacksTo(16), 5));
+
+    /** Candy **/
+
+    public static final RegistryObject<Item> CANDY1 = ITEMS.register("candy1", () -> new Item(new Item.Properties()
+            .stacksTo(3).food(ModFoods.CANDY)));
+
+    public static final RegistryObject<Item> CANDY2 = ITEMS.register("candy2", () -> new Item(new Item.Properties()
+            .stacksTo(3).food(ModFoods.CANDY)));
+
+    public static final RegistryObject<Item> CANDY3 = ITEMS.register("candy3", () -> new Item(new Item.Properties()
+            .stacksTo(3).food(ModFoods.CANDY)));
+
+    public static final RegistryObject<Item> CANDY4 = ITEMS.register("candy4", () -> new Item(new Item.Properties()
+            .stacksTo(3).food(ModFoods.CANDY)));
 
     /** Armor Items **/
 
@@ -84,6 +106,12 @@ public class ModItems {
 
     public static final RegistryObject<Item> MOP = ITEMS.register("mop", () -> new MopItem(new ShearsItem.Properties().stacksTo(1).durability(64)));
 
+    public static final RegistryObject<Item> SLEDGEHAMMER = ITEMS.register("sledgehammer", () -> new SledgeHammerItem(Tiers.IRON, 4, 1f,
+            new Item.Properties().durability(100)));
+
+    public static final RegistryObject<Item> INSTAKILL = ITEMS.register("instakill", () -> new InstaKillItem(Tiers.NETHERITE, 99999, -1.0f,
+            new Item.Properties().rarity(Rarity.EPIC).durability(99999)));
+
     public static final RegistryObject<Item> MEDKIT = ITEMS.register("medkit", () -> new MedkitItem(new Item.Properties()
             .stacksTo(15).food(ModFoods.MEDKIT)));
 
@@ -93,47 +121,19 @@ public class ModItems {
     public static final RegistryObject<Item> SCP939DAGGER = ITEMS.register("scp939dagger", () -> new SwordItem(Tiers.WOOD, 5, 1.0f,
             new SwordItem.Properties().durability(110)));
 
-    public static final RegistryObject<Item> INSTAKILL = ITEMS.register("instakill", () -> new InstaKillItem(Tiers.NETHERITE, 99999, -1.0f,
-            new Item.Properties().rarity(Rarity.EPIC).durability(99999)));
+    /** SCP Spawn Eggs **/
 
-    public static final RegistryObject<Item> SLEDGEHAMMER = ITEMS.register("sledgehammer", () -> new SledgeHammerItem(Tiers.IRON, 4, 1f,
-        new Item.Properties().durability(100)));
-
-    /** SCP Items **/
-
-    public static final RegistryObject<Item> SCP268 = ITEMS.register("scp268", () -> new SCP268ArmorItem(ModArmorMaterials.SCP_268, ArmorItem.Type.HELMET, new Item.Properties()));
-
-    public static final RegistryObject<Item> SCP1025 = ITEMS.register("scp1025", () -> new SCP1025Item(new Item.Properties()
-            .stacksTo(1)));
-
-    public static final RegistryObject<ForgeSpawnEggItem> SCP_650_SPAWN_EGG = ITEMS.register("scp_650_spawn_egg", () -> new ForgeSpawnEggItem(ModEntity.SCP_650, 0, 1710618,
+    public static final RegistryObject<ForgeSpawnEggItem> SCP_049_SPAWN_EGG = ITEMS.register("scp_049_spawn_egg", () -> new ForgeSpawnEggItem(ModEntity.SCP_049, 3289650, 1820927,
             new Item.Properties()));
 
     public static final RegistryObject<ForgeSpawnEggItem> SCP_173_SPAWN_EGG = ITEMS.register("scp_173_spawn_egg", () -> new ForgeSpawnEggItem(ModEntity.SCP_173, 15385721, 16721446,
             new Item.Properties()));
 
+    public static final RegistryObject<ForgeSpawnEggItem> SCP_650_SPAWN_EGG = ITEMS.register("scp_650_spawn_egg", () -> new ForgeSpawnEggItem(ModEntity.SCP_650, 0, 1710618,
+            new Item.Properties()));
+
     public static final RegistryObject<ForgeSpawnEggItem> SCP_939_SPAWN_EGG = ITEMS.register("scp_939_spawn_egg", () -> new ForgeSpawnEggItem(ModEntity.SCP_939, 3342336, 10027008,
             new Item.Properties()));
-
-    public static final RegistryObject<ForgeSpawnEggItem> SCP_049_SPAWN_EGG = ITEMS.register("scp_049_spawn_egg", () -> new ForgeSpawnEggItem(ModEntity.SCP_049, 3289650, 1820927,
-            new Item.Properties()));
-
-    public static final RegistryObject<Item> SCP109 = ITEMS.register("scp109", () -> new SCP109Item(new Item.Properties()
-            .stacksTo(1).food(ModFoods.SCP109)));
-
-    /** Candy **/
-    public static final RegistryObject<Item> CANDY1 = ITEMS.register("candy1", () -> new Item(new Item.Properties()
-            .stacksTo(3).food(ModFoods.CANDY)));
-
-    public static final RegistryObject<Item> CANDY2 = ITEMS.register("candy2", () -> new Item(new Item.Properties()
-            .stacksTo(3).food(ModFoods.CANDY)));
-
-    public static final RegistryObject<Item> CANDY3 = ITEMS.register("candy3", () -> new Item(new Item.Properties()
-            .stacksTo(3).food(ModFoods.CANDY)));
-
-    public static final RegistryObject<Item> CANDY4 = ITEMS.register("candy4", () -> new Item(new Item.Properties()
-            .stacksTo(3).food(ModFoods.CANDY)));
-
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
